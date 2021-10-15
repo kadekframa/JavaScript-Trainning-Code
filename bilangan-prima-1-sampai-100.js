9b47531fff5fefa88730509dc7399fcf76722e14
kadekframa <dframa19@gmail.com>
2021-10-16 01:35:11 +0800

    JavaScript Trainning Code, Bilangan Prima dari 1 sampai dengan 100, Semangat!!!

--- bilangan-prima-1-sampai-100.js
+++ bilangan-prima-1-sampai-100.js
@@ -1,5 +1,18 @@
 // Bilangan Prima Kurang dari 100
 
-for(let i = 0; i <= 100; i++){
-     
+
+let tampung_prima = [];
+for(let i = 1; i <= 100; i++){
+    let bilangan = 0;
+    for(let a = 1; a <= i; a++){
+        if(i % a == 0){
+            bilangan = bilangan + 1;
+        }
+    }
+
+    if(bilangan == 2){
+        tampung_prima.push(i);
+    }
 };
+
+document.writeln(`<p>Bilangan Prima Kurang dari 100 adalah : ${tampung_prima}</p>`);
