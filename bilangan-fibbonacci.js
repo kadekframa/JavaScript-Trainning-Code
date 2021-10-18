a653224bde6db75196ec308de1181df2352fa6a4
kadekframa <dframa19@gmail.com>
2021-10-18 23:42:23 +0800

    JavaScript Trainning Code, mencari bilangan fibbonacci dengan JavaScript.Tetap Semangat!

---
+++ bilangan-fibbonacci.js
@@ -0,0 +1,18 @@
+// Bilangan Fibonacci
+
+let tampung_fibbonacci = [];
+let n1 = 0;
+let n2 = 1;
+
+for(let i = 0; i <= 10; i++){
+    if(i === 0 || i === 1){
+        tampung_fibbonacci.push(i);
+    }else{
+        let total = n1 + n2;
+        tampung_fibbonacci.push(total);
+        n1 = n2;
+        n2 = total;
+    }
+}
+
+console.log(tampung_fibbonacci);
