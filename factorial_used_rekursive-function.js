97726be625ab70e0f550e777e2d5928a97024bc3
kadekframa <dframa19@gmail.com>
2021-11-12 14:27:22 +0800

    Javascript trainning code, factorial using recursive function. Tetap Semangat!

---
+++ factorial_used_rekursive-function.js
@@ -0,0 +1,25 @@
+//Kode Program Javascript
+
+const tampil = document.getElementById('tampil');
+function faktorial(){
+    let i, no, fakt;
+    fakt = 1;
+
+    no = document.getElementById("angka");
+    let angka = Number(no.value);
+
+    // Function Rekursive
+    function factorialRecursive(value){
+        if(value === 1){
+            return 1;
+        }else if(value == ""){
+            return "Mohon inputkan terlebih dauhulu bilangannya yaa!";
+        }else{
+            return value * factorialRecursive(value - 1);       // proses pemanggilan method rekursive kembali (Function memanggil dirinya sendiri).
+        }
+    };
+
+    
+    let tampilkan = factorialRecursive(angka);      // memanggil funtion factorialRecursive() dengan berisi parameter berupa inputan angka.
+    tampil.insertAdjacentHTML('afterbegin', `<p>${tampilkan}</p>`);
+};
